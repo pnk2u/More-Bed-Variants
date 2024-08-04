83c6a1457f69ab3b442635fe706ba4484fe0b2dc
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-08-04 04:25:56 +0200

    Initial Commit (1.20.4)

--- src/main/java/de/pnku/mbdv/datagen/MoreBedVariantLootTableGenerator.java
+++ src/main/java/de/pnku/mbdv/datagen/MoreBedVariantLootTableGenerator.java
@@ -2,16 +2,13 @@ package de.pnku.mbdv.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
-import net.minecraft.core.HolderLookup;
 import net.minecraft.world.level.block.Block;
-
-import java.util.concurrent.CompletableFuture;
 
 import static de.pnku.mbdv.init.MbdvBlockInit.more_beds;
 
 public class MoreBedVariantLootTableGenerator extends FabricBlockLootTableProvider {
-    public MoreBedVariantLootTableGenerator(FabricDataOutput dataOutput, CompletableFuture<HolderLookup.Provider> registryLookup) {
-        super(dataOutput, registryLookup);
+    public MoreBedVariantLootTableGenerator(FabricDataOutput dataOutput) {
+        super(dataOutput);
     }
 
     @Override
--- src/main/java/de/pnku/mbdv/datagen/MoreBedVariantRecipeGenerator.java
+++ src/main/java/de/pnku/mbdv/datagen/MoreBedVariantRecipeGenerator.java
@@ -4,23 +4,17 @@ import de.pnku.mbdv.MoreBedVariants;
 import de.pnku.mbdv.block.MoreBedVariantBlock;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.minecraft.core.HolderLookup;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
-import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
-import java.util.concurrent.CompletableFuture;
-
-import static de.pnku.mbdv.datagen.MoreBedVariantTagGenerator.ACACIA_BEDS;
-import static de.pnku.mbdv.datagen.MoreBedVariantTagGenerator.getBedKey;
 import static de.pnku.mbdv.init.MbdvBlockInit.more_beds;
 
 public class MoreBedVariantRecipeGenerator extends FabricRecipeProvider {
-    public MoreBedVariantRecipeGenerator(FabricDataOutput generator, CompletableFuture<HolderLookup.Provider> registryLookup) {
-        super(generator, registryLookup);
+    public MoreBedVariantRecipeGenerator(FabricDataOutput generator) {
+        super(generator);
     }
 
     @Override
@@ -28,9 +22,8 @@ public class MoreBedVariantRecipeGenerator extends FabricRecipeProvider {
         for (Block bedVariantBlock : more_beds) {
             String planksWood = ((MoreBedVariantBlock) bedVariantBlock).bedWoodType;
             Item bedPlanks = ((MoreBedVariantBlock) bedVariantBlock).getPlanksItem(planksWood);
-            String bedColor = ((MoreBedVariantBlock) bedVariantBlock).bedColor;
-            Item bedWool = ((MoreBedVariantBlock) bedVariantBlock).getWoolItem(bedColor);
-            Item bedDye = ((MoreBedVariantBlock) bedVariantBlock).getDyeItem(bedColor);
+            String woolColor = ((MoreBedVariantBlock) bedVariantBlock).bedColor;
+            Item bedWool = ((MoreBedVariantBlock) bedVariantBlock).getWoolItem(woolColor);
             ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, bedVariantBlock.asItem())
                     .unlockedBy("has_planks", has(bedPlanks))
                     .unlockedBy("has_wool", has(bedWool))
@@ -38,15 +31,7 @@ public class MoreBedVariantRecipeGenerator extends FabricRecipeProvider {
                     .pattern("___")
                     .define('W', bedWool)
                     .define('_', bedPlanks)
-                    .save(recipeOutput, MoreBedVariants.asId(planksWood + "_" + bedColor + "_bed"));
-
-            ShapelessRecipeBuilder.shapeless(RecipeCategory.DECORATIONS, bedVariantBlock.asItem())
-                    .unlockedBy("has_bed_type", has(getBedKey(planksWood)))
-                    .unlockedBy("has_dye", has(bedDye))
-                    .requires(getBedKey(planksWood))
-                    .requires(bedDye)
-                    .save(recipeOutput, MoreBedVariants.asId(planksWood + "_" + bedColor + "_bed_from_dye"));
-
+                    .save(recipeOutput, MoreBedVariants.asId(planksWood + "_" + woolColor + "_bed"));
         }
     }
 }
