ec069acce180aa371f4b439ab17c37ed12818bae
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-08-04 04:25:56 +0200

    Initial Commit (1.20.4)

--- src/main/java/de/pnku/mbdv/datagen/MoreBedVariantLangGenerator.java
+++ src/main/java/de/pnku/mbdv/datagen/MoreBedVariantLangGenerator.java
@@ -4,19 +4,16 @@ import de.pnku.mbdv.block.MoreBedVariantBlock;
 import de.pnku.mbdv.init.MbdvBlockInit;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
-import net.minecraft.core.HolderLookup;
 import net.minecraft.world.level.block.Block;
 import org.apache.commons.lang3.text.WordUtils;
 
-import java.util.concurrent.CompletableFuture;
-
 public class MoreBedVariantLangGenerator extends FabricLanguageProvider {
-    public MoreBedVariantLangGenerator(FabricDataOutput dataOutput, CompletableFuture<HolderLookup.Provider> registryLookup) {
-        super(dataOutput, registryLookup);
+    public MoreBedVariantLangGenerator(FabricDataOutput dataOutput) {
+        super(dataOutput);
     }
 
     @Override
-    public void generateTranslations(HolderLookup.Provider registryLookup, TranslationBuilder translationBuilder) {
+    public void generateTranslations(TranslationBuilder translationBuilder) {
         for (Block bed : MbdvBlockInit.more_beds) {
             String bedName = WordUtils.capitalizeFully(((MoreBedVariantBlock) bed).bedColor + " " + ((MoreBedVariantBlock) bed).bedWoodType) + " Bed";
             translationBuilder.add(bed, bedName);
--- src/main/java/de/pnku/mbdv/datagen/MoreBedVariantRecipeGenerator.java
+++ src/main/java/de/pnku/mbdv/datagen/MoreBedVariantRecipeGenerator.java
@@ -4,20 +4,17 @@ import de.pnku.mbdv.MoreBedVariants;
 import de.pnku.mbdv.block.MoreBedVariantBlock;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.minecraft.core.HolderLookup;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
-import java.util.concurrent.CompletableFuture;
-
 import static de.pnku.mbdv.init.MbdvBlockInit.more_beds;
 
 public class MoreBedVariantRecipeGenerator extends FabricRecipeProvider {
-    public MoreBedVariantRecipeGenerator(FabricDataOutput generator, CompletableFuture<HolderLookup.Provider> registryLookup) {
-        super(generator, registryLookup);
+    public MoreBedVariantRecipeGenerator(FabricDataOutput generator) {
+        super(generator);
     }
 
     @Override
