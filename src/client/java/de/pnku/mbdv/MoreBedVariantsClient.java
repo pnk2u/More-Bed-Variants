8739c7ce85999381d363879dbc2b614e11e7e644
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-11-30 06:42:28 +0100

    Initial Commit (1.21.4)

--- src/client/java/de/pnku/mbdv/MoreBedVariantsClient.java
+++ src/client/java/de/pnku/mbdv/MoreBedVariantsClient.java
@@ -1,25 +1,15 @@
 package de.pnku.mbdv;
 
 import de.pnku.mbdv.init.MbdvBlockInit;
-import de.pnku.mbdv.init.MbdvItemInit;
-import de.pnku.mbdv.renderer.MoreBedVariantDynamicItemRenderer;
 import de.pnku.mbdv.renderer.MoreBedVariantRenderer;
 import de.pnku.mbdv.ui.MbdvCreativeTab;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
-import net.minecraft.world.item.BlockItem;
-
 public class MoreBedVariantsClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
         BlockEntityRenderers.register(MbdvBlockInit.MORE_BED_VARIANT_BLOCK_ENTITY, MoreBedVariantRenderer::new);
-
-        for (BlockItem mbdvBedItem : MbdvItemInit.more_bed_items) {
-            BuiltinItemRendererRegistry.INSTANCE.register(mbdvBedItem,
-                    new MoreBedVariantDynamicItemRenderer());
-        }
         MbdvCreativeTab.registerMbdvCreativeTab();
     }
 }
--- src/main/java/de/pnku/mbdv/datagen/MoreBedVariantBlockStateGenerator.java
+++ src/main/java/de/pnku/mbdv/datagen/MoreBedVariantBlockStateGenerator.java
@@ -1,29 +1,29 @@
-package de.pnku.mbdv.datagen;
-
-import de.pnku.mbdv.block.MoreBedVariantBlock;
-import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-import net.minecraft.data.models.BlockModelGenerators;
-import net.minecraft.data.models.ItemModelGenerators;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-
-import static de.pnku.mbdv.init.MbdvBlockInit.more_beds;
-
-public class MoreBedVariantBlockStateGenerator extends FabricModelProvider {
-    public MoreBedVariantBlockStateGenerator(FabricDataOutput generator) {
-        super(generator);
-    }
-
-    @Override
-    public void generateBlockStateModels(BlockModelGenerators blockStateModelGenerator) {
-    for (Block bedBlock : more_beds) {
-            blockStateModelGenerator.createBedItem(bedBlock,((MoreBedVariantBlock) bedBlock).getWoolBlock(((MoreBedVariantBlock) bedBlock).bedColor));
-            blockStateModelGenerator.createNonTemplateModelBlock(bedBlock, Blocks.WHITE_BED);
-    }
-    }
-
-    @Override
-    public void generateItemModels(ItemModelGenerators itemModelGenerator) {
-    }
-}
+//package de.pnku.mbdv.datagen;
+//
+//import de.pnku.mbdv.block.MoreBedVariantBlock;
+//import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+//import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+//import net.minecraft.data.models.BlockModelGenerators;
+//import net.minecraft.data.models.ItemModelGenerators;
+//import net.minecraft.world.level.block.Block;
+//import net.minecraft.world.level.block.Blocks;
+//
+//import static de.pnku.mbdv.init.MbdvBlockInit.more_beds;
+//
+//public class MoreBedVariantBlockStateGenerator extends FabricModelProvider {
+//    public MoreBedVariantBlockStateGenerator(FabricDataOutput generator) {
+//        super(generator);
+//    }
+//
+//    @Override
+//    public void generateBlockStateModels(BlockModelGenerators blockStateModelGenerator) {
+//    for (Block bedBlock : more_beds) {
+//            blockStateModelGenerator.createBedItem(bedBlock,((MoreBedVariantBlock) bedBlock).getWoolBlock(((MoreBedVariantBlock) bedBlock).bedColor));
+//            blockStateModelGenerator.createNonTemplateModelBlock(bedBlock, Blocks.WHITE_BED);
+//    }
+//    }
+//
+//    @Override
+//    public void generateItemModels(ItemModelGenerators itemModelGenerator) {
+//    }
+//}
