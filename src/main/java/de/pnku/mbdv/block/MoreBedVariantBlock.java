02b489b68e0577a97aa42f877c5085ebd45f1bc3
pnk2u <159317511+pnk2u@users.noreply.github.com>
2025-05-05 09:59:29 +0200

    Update dep

--- src/main/java/de/pnku/mbdv/block/MoreBedVariantBlock.java
+++ src/main/java/de/pnku/mbdv/block/MoreBedVariantBlock.java
@@ -16,6 +16,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BedPart;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
