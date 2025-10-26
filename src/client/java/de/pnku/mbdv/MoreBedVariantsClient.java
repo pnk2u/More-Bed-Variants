901dd46de1fc488af1885f8e4db634834599090e
pnku <159317511+pnk2u@users.noreply.github.com>
2025-10-26 04:35:30 +0100

    Adapt Bedspreads Mixin to 1.20.1 (2.1.0|10)

--- src/client/java/de/pnku/mbdv/MoreBedVariantsClient.java
+++ src/client/java/de/pnku/mbdv/MoreBedVariantsClient.java
@@ -69,6 +69,6 @@ public class MoreBedVariantsClient implements ClientModInitializer {
     }
 
     public static ResourceLocation withModId(String path) {
-        return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
+        return new ResourceLocation(MOD_ID, path);
     }
 }
--- src/client/java/de/pnku/mbdv/mixin/compat/bedspreads/client/DecoratedBedBlockEntityRendererMixin.java
+++ src/client/java/de/pnku/mbdv/mixin/compat/bedspreads/client/DecoratedBedBlockEntityRendererMixin.java
@@ -1,35 +1,52 @@
 package de.pnku.mbdv.mixin.compat.bedspreads.client;
 
 import com.illusivesoulworks.bedspreads.client.DecoratedBedBlockEntityRenderer;
-import com.illusivesoulworks.bedspreads.common.BedspreadsData;
-import com.illusivesoulworks.bedspreads.common.BedspreadsRegistry;
 import com.illusivesoulworks.bedspreads.common.DecoratedBedBlockEntity;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.blaze3d.vertex.PoseStack;
 import de.pnku.mbdv.MoreBedVariantsClient;
 import de.pnku.mbdv.block.MoreBedVariantBlock;
-import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.core.Direction;
-import net.minecraft.core.component.DataComponentType;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.BedItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BannerPatternLayers;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(DecoratedBedBlockEntityRenderer.class)
 public abstract class DecoratedBedBlockEntityRendererMixin {
 
-    @WrapOperation(method = "renderPiece", at = @At(value = "INVOKE", target = "Lnet/minecraft/resources/ResourceLocation;fromNamespaceAndPath(Ljava/lang/String;Ljava/lang/String;)Lnet/minecraft/resources/ResourceLocation;"))
-    ResourceLocation wrappedFromNamespaceAndPathInRenderPiece(String namespace, String path, Operation<ResourceLocation> original, PoseStack ps, MultiBufferSource mbs, ModelPart mp, Direction d, int l, int o, boolean h, BannerPatternLayers bpl, DecoratedBedBlockEntity bedEntity) {
-        Block bedBlock = ((BlockItem)((BedspreadsData) bedEntity.getItem().get(BedspreadsRegistry.BEDSPREADS_DATA.get())).bed().getItem()).getBlock();
+    @Unique
+    String mbedv$bedVariant = "";
+
+    @Inject(method = "render(Lcom/illusivesoulworks/bedspreads/common/DecoratedBedBlockEntity;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;II)V",
+            at = @At(value = "INVOKE", target = "Lcom/illusivesoulworks/bedspreads/client/DecoratedBedBlockEntityRenderer;renderPiece(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;Lnet/minecraft/client/model/geom/ModelPart;Lnet/minecraft/core/Direction;IIZLjava/util/List;)V"))
+    void injectedRenderAtRenderPieceBefore(DecoratedBedBlockEntity blockEntity, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int light, int overlay, CallbackInfo ci) {
+        setBedVariantFromDecoratedBlockEntity(blockEntity);
+    }
+
+    @Unique
+    void setBedVariantFromDecoratedBlockEntity(DecoratedBedBlockEntity bedBlockEntity) {
+        ItemStack bedStack = bedBlockEntity.getItem().getTagElement("BlockEntityTag").contains("BedStack")
+                ? ItemStack.of(bedBlockEntity.getItem().getTagElement("BlockEntityTag").getCompound("BedStack"))
+                : ItemStack.EMPTY;
+        Block bedBlock = ((BedItem) bedStack.getItem()).getBlock();
         if (bedBlock instanceof MoreBedVariantBlock moreBedVariantBlock) {
-            String bedVariant = moreBedVariantBlock.bedWoodType.replace("stripped_", "").replace("bound_", ""); // (Stripped) Bound Bamboo Beds use regular bamboo variant
-            String newPath = "entity/bed/" + bedVariant;
+            this.mbedv$bedVariant = moreBedVariantBlock.bedWoodType.replace("stripped_", "").replace("bound_", ""); // (Stripped) Bound Bamboo Beds use regular bamboo variant
+        } else {
+            this.mbedv$bedVariant = "";
+        }
+    }
+
+    @WrapOperation(method = "renderPiece", at = @At(value = "NEW", target = "Lnet/minecraft/resources/ResourceLocation;"))
+    ResourceLocation wrappedFromNamespaceAndPathInRenderPiece(String namespace, String path, Operation<ResourceLocation> original) {
+        if (!this.mbedv$bedVariant.isEmpty()) {
+            String newPath = "entity/bed/" + this.mbedv$bedVariant;
             return original.call(MoreBedVariantsClient.MOD_ID, newPath);
         } else {
             return original.call(namespace, path);
